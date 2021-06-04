6d747ec63bfeb05c92eec08a0a3655a298a031c0
samjbruce <samueljamesbruce@gmail.com>
2021-06-04 09:01:27 -0400

    adds movie vue app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Movies</h1>
+    <button v-on:click="createProducts">Add Movie</button><br>
+    Title: <input type="text" v-model="inputTitle"><br>
+    Plot: <input type="text" v-model="inputPlot"><br>
+    Year: <input type="text" v-model="inputYear"><br>
+    Director: <input type="text" v-model="inputDirector"><br>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h3>Title: {{ movie.title }}</h3>
+      <p> {{ movie.plot }}</p>
+      <p>Release Date: {{ movie.year }}</p>
+      <p>Director: {{ movie.director }}</p>
+      <button v-on:click="showMovie(movie)">More Info</button>
+    </div>
+        <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info</h1>
+        <img src="" alt="" />
+        <p>Title: ...</p>
+        <p>Plot: ...</p>
+        <p>Year: ...</p>
+        <p>Director: ...</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        movies: [],
+        inputTitle: "",
+        inputPlot: "",
+        inputYear: "",
+        inputDirector: ""
+      };
+    },
+    created: function () {
+      this.indexProducts();
+    },
+    methods: {
+      indexProducts: function () {
+        axios.get("http://localhost:3000/movies")
+        .then((response) => {
+          console.log(response.data);
+          this.movies = response.data;
+        });
+      },
+      createProducts: function () {
+        var params = {
+          title: this.inputTitle,
+          plot: this.inputPlot,
+          year: this.inputYear,
+          director: this.inputDirector
+        };
+        axios.post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors)
+        }),
+        this.inputTitle = "";
+        this.inputPlot = "";
+        this.inputYear = "";
+        this.inputDirector = "";
+      },
+      showMovie: function (movie) {
+        console.log(movie);
+        document.querySelector('#movie-details').showModal();
+      }
+    }
+  }
 </script>
